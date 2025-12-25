8471d217ad43579ef73a59542e989994a4433b35
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2025-12-25 15:42:34 +0200

    refactor(tennis-game2): refactor remove score is zero check as it's covered on non equal scores cases

--- php/src/TennisGame2.php
+++ php/src/TennisGame2.php
@@ -27,18 +27,6 @@ class TennisGame2 implements TennisGame
             $score = $this->convertEqualityScoreToString($this->player1Point);
         }
 
-        if ($this->player1Point > 0 && $this->player2Point === 0) {
-            $this->player1Result = $this->convertScoreToString($this->player1Point);
-            $this->player2Result = 'Love';
-            $score = "{$this->player1Result}-{$this->player2Result}";
-        }
-
-        if ($this->player2Point > 0 && $this->player1Point === 0) {
-            $this->player2Result = $this->convertScoreToString($this->player2Point);
-            $this->player1Result = 'Love';
-            $score = "{$this->player1Result}-{$this->player2Result}";
-        }
-
         if ($this->player1Point > $this->player2Point && $this->player1Point < 4) {
             $this->player1Result = $this->convertScoreToString($this->player1Point);
             $this->player2Result = $this->convertScoreToString($this->player2Point);
